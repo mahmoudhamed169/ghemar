a69f9d8ad41d1a6dfa3153def9a8371ed0ed341b
Mahmoud Hamed <mahmoud.hamed.shenawy@gmail.com>
2026-05-10 15:29:45 +0300

    fix the problem of the image

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
 export default createMiddleware(routing);
-
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|images|favicon.ico).*)"],
 };
